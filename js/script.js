ba7d248ef9c722d5264a3e46dd339bb0fd627ffa
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-11-21 16:50:17 -0300

    Adição do pattern e inicio da criação de uma função para verificação do formulário

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Selecionando todos elementos required
+
+const camposDoFormulario = document.querySelectorAll("[required]");
+
+camposDoFormulario.forEach ((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo));
+})
+
+function verificaCampo(campo) {
+
+}
