28106f555394532bd6e985d707b9aa67db6a6152
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-11-22 16:07:08 -0300

    Criação das funçoes para validar numero repetidos e primeiro digito verificador

--- js/validaCpf.js
+++ js/validaCpf.js
@@ -2,5 +2,43 @@
 
 export default function removePt (campo) {
     const cpf = campo.value.replace(/\.|-/g,"");
-    console.log(cpf);
+    validaNumerosRepetidos(cpf);
+    validaPrimeiroDigito(cpf);
+
+    console.log(validaNumerosRepetidos(cpf));
+}
+
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito (cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let tamanho = 0; tamanho < 9; tamanho++) {
+        soma+= cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma ==10 || soma ==11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
 }
